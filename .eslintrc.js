20eaa9bae2bdf971d38a6d8b6c4505063a673002
NickSettler <moiseevnikita14@gmail.com>
2021-02-16 22:06:02 +0500

    adds eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,15 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-var": "error",
+    semi: "error",
+    indent: "error",
+    "no-multi-spaces": "error",
+    "space-in-parens": "error",
+    "no-multiple-empty-lines": "error",
+    "prefer-const": "error",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"]
   }
 };
