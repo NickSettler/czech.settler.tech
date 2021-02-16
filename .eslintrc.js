83b1a6af88600be3e9f6bcf45bd53ca137a5686c
NickSettler <moiseevnikita14@gmail.com>
2021-02-16 22:26:32 +0500

    changes eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-var": "error",
-    semi: "error",
-    indent: "error",
-    "no-multi-spaces": "error",
-    "space-in-parens": "error",
-    "no-multiple-empty-lines": "error",
-    "prefer-const": "error",
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"]
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/essential",
+        '@vue/typescript',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    }
 };
