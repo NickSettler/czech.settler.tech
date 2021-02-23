cee8a5689faaa73a7e395b0f1959c6b7fde63716
NickSettler <moiseevnikita14@gmail.com>
2021-02-23 15:02:24 +0500

    adds store functionality

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,9 +4,26 @@ import Api from '@/classes/api';
 
 Vue.use(Vuex);
 
+const authModule: StoreOptions<any> = {
+    state: {
+        logged: Api.getInstance().auth.token !== null,
+        userData: {},
+    },
+    mutations: {
+        setLogged: (state, logged) => (state.logged = logged),
+        setUserData: (state, data) => (state.userData = data),
+    },
+    actions: {
+        setLogged: ({ commit }) => commit('setLogged', Api.getInstance().auth.token !== null),
+        setUserData: async ({ commit }) => commit('setUserData', (await Api.getInstance().users.me.read()).data),
+    },
+};
+
 export default new Vuex.Store({
     state: {},
     mutations: {},
     actions: {},
-    modules: {},
+    modules: {
+        auth: authModule,
+    },
 });
