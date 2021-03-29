ccacb44053dfc1b9af466026c102ac7a6d760ac8
NickSettler <moiseevnikita14@gmail.com>
2021-03-29 23:25:33 +0500

    changes icons and manifest settings

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,11 @@ module.exports = {
 
     pwa: {
         name: 'Český Slovník',
+        themeColor: '#003087',
+        msTileColor: '#003087',
+        manifestOptions: {
+            description: 'App for creating list of words to remember',
+            background_color: '#003087',
+        },
     },
 };
